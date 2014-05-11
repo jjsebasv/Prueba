94c8f605eb043e8f0263b18c9fca734992514630
jjSebasV <sebastian.vera@colegiochampagnat.com.ar>
2014-05-11 14:11:08 -0300

    probando

---
+++ TrabajoPoo/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Prueba aa = new Prueba();
+		aa.pruebita();
+		
+	}
+
+}
---
+++ TrabajoPoo/src/Prueba.java
@@ -0,0 +1,11 @@
+
+public class Prueba {
+
+	
+	Integer numero = 9;
+	
+	public void pruebita(){
+		this.numero += 3;
+		System.out.println("esto es una prueba, " + numero);
+	}
+}
